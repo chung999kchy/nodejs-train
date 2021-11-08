4a69ee1f51b3800f8921881fd30c53ed97c177c1
chung999kchy <chung999kc@gmail.com>
2021-11-08 13:38:03 +0700

    [feat] add prettier, husky, lint-staged and router, controller

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-var handlebars = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+var handlebars = require('express-handlebars');
 const app = express();
 const port = 3008;
 
-app.use(express.static(path.join(__dirname, "public")));
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.json());
 
 // HTTP Logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-	"hbs",
+	'hbs',
 	handlebars({
-		extname: ".hbs",
+		extname: '.hbs',
 	})
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get("/", (req, res) => {
-	res.render("home");
-});
+// Route init
+route(app);
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
